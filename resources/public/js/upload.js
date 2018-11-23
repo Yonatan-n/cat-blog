7033715d0e03c7573fa824cef771a55d9e433b15
yonatan-n <yonatan.n@protonmail.com>
2018-11-23 22:19:01 +0200

    added fileReader thing to the /upload form. now showing a small image after upload.

--- resources/public/js/upload.js
+++ resources/public/js/upload.js
@@ -2,3 +2,28 @@ console.log('upload js')
 function clearForm () {
   document.querySelector('form').reset()
 }
+function gotImgHere (event) {
+  const pic = event.target.files[0]
+  const reader = new FileReader()
+  console.log(event)
+  let parent = event.target.parentElement
+  let picElem = document.createElement('div')
+  picElem.classList.add('jumbotron')
+  /* picElem.style.maxWidth = '100%'
+  picElem.style.maxHeight = '100%' */
+  reader.onload = function (event2) {
+    picElem.innerHTML = `
+      <img src='${event2.target.result}' class='center' style="max-width: 100%; max-height: 100%;">`
+    if (parent.childElementCount === 1) {
+      parent.appendChild(picElem)
+    } else {
+      parent.children[1].children[0].src = event2.target.result
+    }
+    // event.target.parentElement.appendChild(picElem)
+    // console.log(event.target)
+
+    // ls.appendChild(picElem)
+    // console.log(event2.target.result, 'inner thing here!!!')
+  }
+  reader.readAsDataURL(pic)
+}
